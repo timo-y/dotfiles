1ce0f0779972dc612753764198aac6dcf846ea87
Timo Yu <ty@21globo.com>
2025-11-14 16:49:11 +0100

    oh my zsh installer -y

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ else
 fi
 
 # ── install oh-my-zsh ─────────────────────────────────────────────────
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 #          ╭──────────────────────────────────────────────────────────╮
 #          │                          Config                          │
